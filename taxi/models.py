f291fa865a7e10647c6f7bf97b33499eb5ec8496
Artem <stilon670@gmail.com>
2025-09-30 13:01:26 +0300

    code style flake8

--- taxi/models.py
+++ taxi/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=100, unique=True)
 
