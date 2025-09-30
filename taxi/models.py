c33d2d41fc84a3c7422f964f1ca49baaa303506f
Artem <stilon670@gmail.com>
2025-09-30 12:14:36 +0300

    Add models and made migrate

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["username"]
+
+    def str(self):
+        return f"{self.username}, license: {self.license_number}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        "Manufacturer", on_delete=models.CASCADE, related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        Driver, related_name="cars"
+    )
+
+    class Meta:
+        ordering = ["model"]
+
+    def __str__(self):
+        return self.model
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
